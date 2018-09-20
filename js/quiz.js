a5ef1caea0b31866b19562d413a4762f50e08183
Marco van Setten <s1114352@student.windesheim.nl>
2018-09-20 13:36:18 +0200

    test van Jan

--- js/quiz.js
+++ js/quiz.js
@@ -6,6 +6,10 @@
 function addButtonActions() {
     var startButton = document.getElementById('button-start');
     var questionsButton = document.getElementById('button-questions');
+    var AntwoordButtoneen = document.getElementById('button-1');
+    var AntwoordButtontwee = document.getElementById('button-2');
+    var AntwoordButtondrie = document.getElementById('button-3');
+    var AntwoordButtonvier = document.getElementById('button-4');
 
     startButton.addEventListener("click", function () {
         showStartPage();
@@ -13,17 +17,31 @@ function addButtonActions() {
     questionsButton.addEventListener("click", function () {
         showQuestionsPage();
     });
+    AntwoordButtoneen.addEventListener("click", function () {
+        showAntwoordPage();
+    });
+    AntwoordButtontwee.addEventListener("click", function () {
+        showWrongPage();
+    });
+    AntwoordButtondrie.addEventListener("click", function () {
+        showWrongPage();
+    });
+    AntwoordButtonvier.addEventListener("click", function () {
+        showWrongPage();
+    });
 }
+
+
 
 /**
  * Hide all pages
  */
 function hideAllPages() {
     var startPage = document.getElementById('page-start');
-    var questionsPage = document.getElementById('page-questions');
-
+    var AntwoordPage = document.getElementById('page-Antwoord');
+   
     startPage.style.display = 'none';   
-    questionsPage.style.display = 'none';
+    AntwoordPage.style.display = 'none';
 }
 
 /**
@@ -43,13 +61,36 @@ function showStartPage() {
  * Show questions page
  */
 function showQuestionsPage() {
-    var page = document.getElementById('page-questions');
+    var page = document.getElementById('page-Antwoord');
     
     hideAllPages();
 
     page.style.display = 'block';
 
-    console.info('Je bent nu op de vragenpagina');
+    window.alert('Je bent nu op de vragenpagina');
+}
+
+/**
+ * Show Antwoord page
+ */
+function showAntwoordPage(gegevenAntwoord) {
+    var page = document.getElementById('page-Antwoord');
+    
+    hideAllPages();
+
+    page.style.display = 'block';
+
+    window.alert('je hebt het antwoord goed');
+}
+
+function showWrongPage() {
+    var page = document.getElementById('page-Antwoord');
+    
+    hideAllPages();
+
+    page.style.display = 'block';
+
+    window.alert('je hebt het antwoord Fout');
 }
 
 // Initialize
