36a789086324f798d666e0a8a2b5ffed2949bc39
Marco van Setten <s1114352@student.windesheim.nl>
2018-10-11 13:50:40 +0200

    VERERGAAN

--- js/quiz.js
+++ js/quiz.js
@@ -143,9 +143,40 @@ function checkStudent() {
         //studentIdentificationFailed(xHttp.statusText);
     };
     xHttp.open("GET", "https://quiz.clow.nl/v1/student/" + studentNummer, true);
-    xHttp.send();
+    xHttp.send(
+    );
     
 }
+
+function saveResultaten() {
+    
+    var studentNummer = document.getElementById('studentnummer').value;
+    var xHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange = function () {
+        if (xHttp.readyState == XMLHttpRequest.DONE) {
+            var response = JSON.parse(xHttp.response);
+            if (xHttp.status == 200) {
+                studentIdentificationSucces(response);
+            } else {
+                studentIdentificationFailed(response);
+            }
+        }
+    };
+    xHttp.onerror = function () {
+        //studentIdentificationFailed(xHttp.statusText);
+    };
+    xHttp.open("POST", "https://quiz.clow.nl/v1/score + studentNummer, true);
+    xHttp.send();"POST", "https://quiz.clow.nl/v1/score",
+        JSON.stringify( {
+            quizMaster: Master,
+            Student : studentNummer,
+            points: score,
+            time : 0,
+} )
+);
+
+
+
 
 /**
  * Student is successfully identified
@@ -156,13 +187,13 @@ function studentIdentificationSucces(student) {
     var page = document.getElementById('page-Antwoord');
     
     hideAllPages();
-
+    
     page.style.display = 'block';
     
     alert("welkom" + " " + voornaam + " " + achterNaam);
-
+    
     var naamVragenquiz =document.getElementById("NaamVerschijning")
-
+    
     naamVragenquiz.innerHTML = "Succes met de Quiz" + " " + (student.firstName + " " + student.lastName);
     
 }
@@ -171,25 +202,25 @@ function studentIdentificationSucces(student) {
  * Student number is incorrect
  */
 function studentIdentificationFailed() {
-   
-alert("Foute inlog gegevens");
+    
+    alert("Foute inlog gegevens");
 }
 
 /**
-* Antwoorden
-*/
+ * Antwoorden
+ */
 var kies = [1, 2, 3, 4];
 var juiste = [0, 1, 3, 2, 3, 1, 1, 3, 0, 0, 2, 3, 1, 0, 2, 3, 0, 1, 2, 1];
 var i = 0;
 var score = 0;
-
+var Master = "S1114352";
 /**
-* Array's
-*/
+ * Array's
+ */
 var vragen = ["Waar gebruik je HTML voor?", "Waar gebruik je CSS voor?", "Waar gebruik je JavaScript voor?", "Hoe kopieer je een geselecteerde tekst?", "Hoe plak je een tekst?",
-    "Hoe verander je de tekst kleur?", "Hoe undo je een actie?", "Hoe redo je een actie?", "Hoe knip je een actie weg?", "Welke button position is gebruikt voor deze quiz?",
-    "Hoe vaak kan je dezelfde var naam gebruiken?", "Hoe verhoog je een var = 0?", "Waar gebruik je een div voor?", "Hoe align je een tekst?", "Hoe maak je een header tag?",
-    "Hoe maak je een array?", "Waar staat html voor?", "Waar gebruik je margin voor?", "Hoe voeg je comments toe in JavaScript?", "Hoe begin je een HTML document?"];
+"Hoe verander je de tekst kleur?", "Hoe undo je een actie?", "Hoe redo je een actie?", "Hoe knip je een actie weg?", "Welke button position is gebruikt voor deze quiz?",
+"Hoe vaak kan je dezelfde var naam gebruiken?", "Hoe verhoog je een var = 0?", "Waar gebruik je een div voor?", "Hoe align je een tekst?", "Hoe maak je een header tag?",
+"Hoe maak je een array?", "Waar staat html voor?", "Waar gebruik je margin voor?", "Hoe voeg je comments toe in JavaScript?", "Hoe begin je een HTML document?"];
 
 //Lijst
 var antwoordLijst = [
@@ -213,14 +244,23 @@ var antwoordLijst = [
     ["Om ruimte te creëren in de echte wereld", "Om ruimte te creëren rond om de elementen", "Om ruimte weg te halen", "Om elementen te creëren"],
     ["*// */", "**/ */", "/** */", "*/ /*"],
     ["DOCTYPE html", "!DOCTYPE html", "!!DOCTYPE html", "DOCTYPE html !"]
-
+    
 ];
 
 var aantalVragen = ["Vraag 1/20", "Vraag 2/20", "Vraag 3/20", "Vraag 4/20", "Vraag 5/20", "Vraag 6/20", "Vraag 7/20", "Vraag 8/20", "Vraag 9/20", "Vraag 10/20", "Vraag 11/20", "Vraag 12/20", "Vraag 13/20", "Vraag 14/20", "Vraag 15/20", "Vraag 16/20", "Vraag 17/20", "Vraag 18/20", "Vraag 19/20", "Vraag 20/20"]; //derde lijst
 
 /**
-* Intializes
-*/
+ * Intializes
+ */
 addButtonActions();
 showStartPage();
 antwoord();
+
+/*xHttp.send();"POST", "https://quiz.clow.nl/v1/score",
+JSON.stringify( {
+    quizMaster: Master,
+    Student : studentNummer,
+    points: score,
+    time : 0,
+} )
+);*/
